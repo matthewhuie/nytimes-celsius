501f1b719b60d3ee9e29014479183cdc7407a033
Matthew Huie <matthew@matthewhuie.com>
2021-07-04 13:39:12 -0400

    Adding comments to nytimes-celsius.js

--- nytimes-celsius.js
+++ nytimes-celsius.js
@@ -1,4 +1,7 @@
+// Use MutationObserver to watch element that loads with page in DOM
 $("section#masthead-bar-one div#masthead-bar-one-widgets").observe('childlist subtree', record => {
+
+  // Main weather widget element
   weather_widget = $("div[data-testid='weather-widget']:not(.nytimes-celsius)");
 
   if (weather_widget.length) {
@@ -7,6 +10,7 @@ $("section#masthead-bar-one div#masthead-bar-one-widgets").observe('childlist su
     regex = /[0-9]+°[F]?/g
     temps_f = weather_html.match(regex);
 
+    // Performs unit conversion while maintaining suffixes, if present
     temps_c = temps_f.forEach(temp_f => {
       suffix = temp_f.replace(/^[0-9]+/, '').replace('F', 'C');
       value = temp_f.replace(/°[F]?/, '');
@@ -16,6 +20,7 @@ $("section#masthead-bar-one div#masthead-bar-one-widgets").observe('childlist su
       weather_html = weather_html.replaceAll(temp_f, temp_c + suffix);
     });
 
+    // Pushes changes to DOM and adds watermark
     weather_widget.html(weather_html);
     weather_widget.addClass('nytimes-celsius');
   }
